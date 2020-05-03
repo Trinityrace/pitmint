0103917b3807e74549b52931fb1267b63745eda5
Trinity John <trinityrtime@gmail.com>
2020-05-04 01:07:02 +0300

    importing the SQLAlchemy and creating a db instance.

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
+# ...
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # ....
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    # ...
+
+    # We will next consider how we can create models
+    #  for our database. And then we can see how we
+    #  can authenticate users.
