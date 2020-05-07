d31b0074bd04abd6a3d4cff15173a5b60678e5f9
Trinity John <trinityrtime@gmail.com>
2020-05-07 21:22:41 +0300

    profile ready for production amigos

--- manage.py
+++ manage.py
@@ -11,8 +11,8 @@ from flask_migrate import Migrate, MigrateCommand
 
 # create app instance
 
-app = create_app('development')
-#app = create_app('production')
+# app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server', Server)
