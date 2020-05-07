a16c21d88cba5e7599059c2fb67e2eb621e59bdc
Trinity John <trinityrtime@gmail.com>
2020-05-07 11:48:19 +0300

    start.sh file input

---
+++ start.sh
@@ -0,0 +1,7 @@
+ 
+export SECRET_KEY='covid'
+export MAIL_USERNAME='trinityrtime@gmail.com'
+export MAIL_PASSWORD = 'trine420.'
+export DATABASE_URL='postgresql+psycopg2://john:passcoder@localhost/pit'
+
+python manage.py server;
